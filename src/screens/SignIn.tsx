453637c8c97ba0bcef0a4539ebd3883f5e29ccf1
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-02 13:31:33 -0300

    feat: :lipstick: Página SignIn

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Center, Icon } from "native-base";
+import { Center, Icon, Text } from "native-base";
 import { Fontisto } from "@expo/vector-icons";
 
 import { Button } from "../components/Button";
@@ -7,14 +7,20 @@ import Logo from "../assets/logo.svg";
 
 export function SignIn() {
 	return (
-		<Center flex={1} bg="gray.900">
+		<Center flex={1} bg="gray.900" p="7">
 			<Logo />
 
 			<Button
+				type="SECONDARY"
 				title="Entrar com google"
 				leftIcon={<Icon as={Fontisto} name="google" size="md" />}
-				type="SECONDARY"
+				mt={12}
 			/>
+
+			<Text color="white" textAlign="center" mt={4}>
+				Não utilizamos nenhuma informação além {"\n"} do seu e-mail para criação
+				de sua conta.
+			</Text>
 		</Center>
 	);
 }
