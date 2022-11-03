5fc050f672cd2f0a67d1c00b4ae8653c48a4cde2
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-03 11:50:53 -0300

    feat: :sparkles: Autenticação com Google
    Configuração no console.cloud.google, configuração de .env e código de integração no AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,12 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useEffect } from "react";
+import * as Google from "expo-auth-session/providers/google";
+import * as AuthSession from "expo-auth-session";
+import * as WebBrowser from "expo-web-browser";
+
+// @ts-ignore
+import { GOOGLE_CLIENT_ID } from "@env";
+
+WebBrowser.maybeCompleteAuthSession();
 
 interface UserProps {
 	name: string;
@@ -7,6 +15,7 @@ interface UserProps {
 
 export interface AuthContextDataProps {
 	user: UserProps;
+	isUserLoading: boolean;
 	signIn: () => Promise<void>;
 }
 
@@ -17,17 +26,45 @@ interface AuthProviderProps {
 export const AuthContext = createContext({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthProviderProps) {
+	const [user, setUser] = useState<UserProps>({} as UserProps);
+	const [isUserLoading, setIsUserLoading] = useState(false);
+
+	const [request, response, promptAsync] = Google.useAuthRequest({
+		clientId: GOOGLE_CLIENT_ID,
+		redirectUri: AuthSession.makeRedirectUri({
+			useProxy: true
+		}),
+		scopes: ["profile", "email"]
+	});
+
 	async function signIn() {
-		console.log("Vamos Logar!");
+		try {
+			setIsUserLoading(true);
+
+			await promptAsync();
+		} catch (error) {
+			console.log(error);
+			throw error;
+		} finally {
+			setIsUserLoading(false);
+		}
 	}
+
+	async function signInWithGoogle(accessToken: string) {
+		console.log("TOKEN DE AUTENTICAÇÃO ==>", accessToken);
+	}
+
+	useEffect(() => {
+		if (response?.type === "success" && response?.authentication?.accessToken) {
+			signInWithGoogle(response.authentication.accessToken);
+		}
+	}, [response]);
 
 	return (
 		<AuthContext.Provider
 			value={{
-				user: {
-					name: "Caio",
-					avatarUrl: "https://github.com/caiovinicius7.png"
-				},
+				user,
+				isUserLoading,
 				signIn
 			}}
 		>
