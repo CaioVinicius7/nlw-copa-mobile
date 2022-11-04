46db93180a9aeb3f03f0a025b7bc418493d18e72
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-04 12:09:52 -0300

    feat: :lipstick: Customizando bottom tabs

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,4 +1,7 @@
+import { Platform } from "react-native";
+import { useTheme } from "native-base";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { PlusCircle, SoccerBall } from "phosphor-react-native";
 
 import { New } from "../screens/New";
 import { Polls } from "../screens/Polls";
@@ -6,11 +9,46 @@ import { Polls } from "../screens/Polls";
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
-	return (
-		<Navigator>
-			<Screen name="new" component={New} />
+	const { colors, sizes } = useTheme();
 
-			<Screen name="polls" component={Polls} />
+	const size = sizes[6];
+
+	return (
+		<Navigator
+			screenOptions={{
+				headerShown: false,
+				tabBarLabelPosition: "beside-icon",
+				tabBarActiveTintColor: colors.yellow[500],
+				tabBarInactiveTintColor: colors.gray[300],
+				tabBarStyle: {
+					position: "absolute",
+					height: sizes[22],
+					borderTopWidth: 0,
+					backgroundColor: colors.gray[800]
+				},
+				tabBarItemStyle: {
+					position: "relative",
+					top: Platform.OS === "android" ? 0 : 0
+				}
+			}}
+		>
+			<Screen
+				name="new"
+				component={New}
+				options={{
+					tabBarIcon: ({ color }) => <PlusCircle color={color} size={size} />,
+					tabBarLabel: "Novo Bolão"
+				}}
+			/>
+
+			<Screen
+				name="polls"
+				component={Polls}
+				options={{
+					tabBarIcon: ({ color }) => <SoccerBall color={color} size={size} />,
+					tabBarLabel: "Meus Bolões"
+				}}
+			/>
 		</Navigator>
 	);
 }
