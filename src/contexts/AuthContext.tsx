3d0746c18d4f4ee8132893dbe3f4effd15804e82
Caio Vinícius <caio1525pereira@gmail.com>
2022-11-07 18:13:45 -0300

    chore: :wrench: Varáveis de ambiente

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -3,9 +3,6 @@ import * as Google from "expo-auth-session/providers/google";
 import * as AuthSession from "expo-auth-session";
 import * as WebBrowser from "expo-web-browser";
 
-// @ts-ignore
-import { GOOGLE_CLIENT_ID } from "@env";
-
 import { api } from "../services/api";
 
 WebBrowser.maybeCompleteAuthSession();
@@ -32,7 +29,7 @@ export function AuthContextProvider({ children }: AuthProviderProps) {
 	const [isUserLoading, setIsUserLoading] = useState(false);
 
 	const [request, response, promptAsync] = Google.useAuthRequest({
-		clientId: GOOGLE_CLIENT_ID,
+		clientId: process.env.GOOGLE_CLIENT_ID,
 		redirectUri: AuthSession.makeRedirectUri({
 			useProxy: true
 		}),
